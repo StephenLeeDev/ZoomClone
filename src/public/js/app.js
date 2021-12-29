dcc10af798d6421e26b8d84c909fae0556136e81
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-12-29 21:39:53 +0900

    Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,6 @@
-const socket = new WebSocket("ws://localhost:3000");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket("ws://192.168.0.125:3000");
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
@@ -12,6 +14,11 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 5000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
